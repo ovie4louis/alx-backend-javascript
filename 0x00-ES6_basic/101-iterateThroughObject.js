96971007305553b42bdd5680d8929f46818ba000
Enakarhire Ovie Louis <enakarhireovie7@gmail.com>
2024-10-01 03:22:07 +0000

    task

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,4 @@
+export default function iterateThroughObject(reportWithIterator) {
+    const names = reportWithIterator.join(" | ");
+    return names;
+}
---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+    const data = {
+        allEmployees: employeesList,
+        getNumberOfDepartments() {
+            return Object.keys(this.allEmployees).length;
+        },
+    };
+    return data;
+}
